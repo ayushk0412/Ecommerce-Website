691520a5e662bc7be3ca5e784ce8101d96178946
ayushk0412 <ayushk0412@gmail.com>
2021-02-13 05:02:33 +0530

    _

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Payment, Item, OrderItem, Order, BillingAddress
-# Register your models here.
-
-admin.site.register(Item)
-admin.site.register(OrderItem)
-admin.site.register(Order)
-admin.site.register(BillingAddress)
-admin.site.register(Payment)
